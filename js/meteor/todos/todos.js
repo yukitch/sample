9c557fc6cf7082a5aaff008274e5fcae1550793b
youkyll <yuuki08180102@gmail.com>
2015-10-02 06:50:28 +0000

    Update meteor todos step5

--- js/meteor/todos/todos.js
+++ js/meteor/todos/todos.js
@@ -26,4 +26,15 @@ if (Meteor.isClient) {
       event.target.text.value = "";
     }
   });
+
+  Template.task.events({
+    "click .toggle-checked": function() {
+      Tasks.update(this._id, {
+        $set: {checked: ! this.checked}
+      });
+    },
+    "click .delete": function() {
+      Tasks.remove(this._id);
+    }
+  });
 }
