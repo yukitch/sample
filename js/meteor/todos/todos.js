d349d01029693d6d35c38120ff31c74c72ead7de
youkyll <yuuki08180102@gmail.com>
2015-10-02 06:16:43 +0000

    Update meteor todos step4

--- js/meteor/todos/todos.js
+++ js/meteor/todos/todos.js
@@ -3,7 +3,27 @@ Tasks = new Mongo.Collection('tasks');
 if (Meteor.isClient) {
   Template.body.helpers({
     tasks: function() {
-      return Tasks.find({});
+      return Tasks.find({}, {sort:{createdAt: -1}});
+    }
+  });
+
+
+  Template.body.events({
+    "submit .new-task": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+
+      // Get value from form element
+      var text = event.target.text.value;
+
+      // Insert a task into the collection
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.text.value = "";
     }
   });
 }
