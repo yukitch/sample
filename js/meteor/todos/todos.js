f5f36d8673d51e780c510ef57bd3c9c908b2648f
youkyll <yuuki08180102@gmail.com>
2015-10-05 09:21:54 +0000

    Update meteor todos step8

--- js/meteor/todos/todos.js
+++ js/meteor/todos/todos.js
@@ -3,7 +3,18 @@ Tasks = new Mongo.Collection('tasks');
 if (Meteor.isClient) {
   Template.body.helpers({
     tasks: function() {
-      return Tasks.find({}, {sort:{createdAt: -1}});
+      if(Session.get("hideCompleted")) {
+        // if hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      }else {
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
     }
   });
 
@@ -24,6 +35,9 @@ if (Meteor.isClient) {
 
       // Clear form
       event.target.text.value = "";
+    },
+    "change .hide-completed input": function(event) {
+      Session.set("hideCompleted", event.target.checked);
     }
   });
 
